21b58cea8a926ef691afe656d67eb4acd8395514
Jefferson Cechinel <jclorets@hotmail.com>
2021-05-12 20:01:08 -0300

    move item searching logic to its own class

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -5,31 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Item;
 use App\Services\Item\Queries\GetItemDetail;
+use App\Services\Item\Queries\Search;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
     public function index(Request $request)
     {
-        $query = Item::query();
-        if ($searchText = $request->input('searchText')) {
-            $query->where('name', 'like', '%' . $searchText . '%');
-            $query->orWhere('description', 'like', '%' . $searchText . '%');
-        }
-
-        $params = $request->all();
-
-        if (isset($params['priceSort'])) {
-            $priceSort = $params['priceSort'];
-
-            if ($priceSort == 'false') {
-                $query->orderBy('final_price', 'desc');
-            } else {
-                $query->orderBy('final_price', 'asc');
-            }
-        }
-
-        $data = $query->paginate(10);
+        $search = new Search();
+        $search->searchText = $request->input('searchText');
+        $search->priceSort = $request->input('priceSort');
+        $data = $search->execute();
 
         return $this->successResponse($data);
     }
---
+++ app/Services/Item/Queries/Search.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services\Item\Queries;
+
+use App\Models\Item;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
+class Search
+{
+    public $userId;
+    public $searchText;
+    public $priceSort;
+    public $perPage = 10;
+
+    const SORT_PRICE_DESC = 'desc';
+    const SORT_PRICE_ASC = 'asc';
+
+    /**
+     * @param $itemId
+     * @return LengthAwarePaginator
+     */
+    public function execute()
+    {
+        $query = Item::query();
+
+        if ($this->searchText) {
+            $query->where('name', 'like', '%' . $this->searchText . '%');
+            $query->orWhere('description', 'like', '%' . $this->searchText . '%');
+        }
+
+        if ($this->priceSort) {
+            $query->orderBy('final_price', $this->priceSort);
+        }
+
+        return $query->paginate($this->perPage);
+    }
+}
